f45e58ab8385f9b5d0947ce66c9c146c42743c2e
Freddyyzyzy <zyan0021@gmail.com>
2025-05-29 01:10:57 +1000

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
+
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World！</h1>');
+// HTML 
+app.use(express.static(path.join(__dirname, "public")));
+
+// 
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+// 
 app.listen(port, () => {
-  console.log(`server run http://localhost:${port}`);
+    console.log(`Server is running at http://localhost:${port}`);
 });
